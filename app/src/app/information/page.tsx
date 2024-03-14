2c3fa9f605b6d716028d982a8085fd9d43c91c8f
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-03-14 21:19:14 +0100

    Location section sketch

--- app/src/app/information/page.tsx
+++ app/src/app/information/page.tsx
@@ -5,7 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ArrowBigRight, MoveRight } from "lucide-react";
+import { MoveRight } from "lucide-react";
 
 export default function Information() {
   return (
@@ -20,14 +20,14 @@ export default function Information() {
       />
       <article>
         <p className="my-auto leading-8">
-          Come join us to a place where you can be yourself! FurGroove AD
+          {`Come join us to a place where you can be yourself! FurGroove AD
           edition welcomes not only furries but also puppies, gearheads and
           everyone with an open mind. Together with{" "}
           <strong className="underline decoration-accent-500 decoration-2">
             Fort33
           </strong>{" "}
           we want to celebrate life and have an awesome tailwagging night with
-          purrfect DJ's and atmosphere!
+          purrfect DJ's and atmosphere!`}
         </p>
         <Card className="mt-6 flex w-full flex-row rounded-md bg-primary-800 px-4 py-2 ">
           <CardContent className="flex flex-col">
--- app/src/app/location/page.tsx
+++ app/src/app/location/page.tsx
@@ -2,9 +2,32 @@ export default function Location() {
   return (
     <section
       id="location"
-      className="mt-96 flex-col gap-6 px-6 lg:w-screen lg:flex-row lg:px-0"
+      className="mb-56 mt-96 flex-col gap-6  px-6 lg:w-screen lg:px-0"
     >
-      <h3>Find us at Fort33 ...or something</h3>
+      <h1 className="ml-0 w-full text-left text-2xl underline decoration-accent-500">
+        FORT33 - Leusden
+      </h1>
+      <div className="flex flex-row">
+        <div className="relative my-auto flex-shrink">
+          <p className="text-md relative ">
+            <span className="absolute left-[-1rem] top-[-2rem]">“</span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip.
+            <span className="absolute bottom-[-3.5rem]">”</span>
+          </p>
+        </div>
+        <div className="hidden h-40 w-40 flex-none rounded-md bg-primary-800 sm:block">
+          FORT33 logo
+        </div>
+      </div>
+
+      <hr className="w-full" />
+      <p className="w-full opacity-50">
+        Bavoortseweg 25, 3833 BM Leusden, Netherlands
+      </p>
+      <div className="h-96 w-full rounded-md bg-primary-800">Location map</div>
     </section>
   );
 }
